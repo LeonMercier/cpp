e59d8921442db27c0833b54fbe32f2a779b85b1d
lemercie <lemercie@student.hive.fi>
2025-01-17 16:50:36 +0200

    styling

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:43:53 by lemercie          #+#    #+#             */
-/*   Updated: 2025/01/15 12:10:29 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/01/17 16:50:10 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 int	main(void)
 {
 	std::string	str = "HI THIS IS BRAIN";
-	std::string	*stringPTR = &str;
-	std::string	&stringREF = str;
+	std::string	*string_ptr = &str;
+	std::string	&string_ref = str;
 
 	std::cout << &str << std::endl;
-	std::cout << stringPTR << std::endl;
-	std::cout << &stringREF << std::endl;
+	std::cout << string_ptr << std::endl;
+	std::cout << &string_ref << std::endl;
 
 	std::cout << str << std::endl;
-	std::cout << *stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << *string_ptr << std::endl;
+	std::cout << string_ref << std::endl;
 }
