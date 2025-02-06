80b64d6b3f4825d6817838ed6f1ede6a80182673
lemercie <lemercie@student.hive.fi>
2025-02-06 15:43:25 +0200

    cpp01/ex05 split strings nicely

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 11:54:42 by lemercie          #+#    #+#             */
-/*   Updated: 2025/01/16 14:33:50 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/06 15:41:59 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,24 @@
 
 void	Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+	std::cout << "I love having extra bacon for my "
+		"7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
 		<< std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+	std::cout << "I cannot believe adding extra bacon costs more money. You "
+		"didn’t put enough bacon in my burger! If you did, I wouldn’t be "
+		"asking for more!"
 		<< std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve "
+		"been coming for years whereas you started working here since last "
+		"month."
 		<< std::endl;
 }
 
--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 11:52:26 by lemercie          #+#    #+#             */
-/*   Updated: 2025/01/16 14:37:29 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/06 15:31:02 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ private:
 	std::string	_lvl_names[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	void (Harl::*_funcs[4])() = {&Harl::debug, &Harl::info, &Harl::warning,
 		&Harl::error};
+
 	void	debug(void);
 	void	info(void);
 	void	warning(void);
