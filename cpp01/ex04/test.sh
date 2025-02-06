57ecee356545a9af0f340a7b0c27655497e23cd2
lemercie <lemercie@student.hive.fi>
2025-02-06 16:15:22 +0200

    make it so that tests dont depend on external files

--- cpp01/ex04/test.sh
+++ cpp01/ex04/test.sh
@@ -6,7 +6,7 @@
 #    By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/06 12:25:49 by lemercie          #+#    #+#              #
-#    Updated: 2025/02/06 15:19:13 by lemercie         ###   ########.fr        #
+#    Updated: 2025/02/06 16:14:23 by lemercie         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,8 @@ outfile2=sed_out.txt
 
 # replaces chars just like sed
 printf "TEST 1\n"
+echo hallo > $infile
+echo hello > $infile
 ./seddy $infile a B
 sed 's/a/B/g' $infile > $outfile2
 if cmp -s $outfile1 $outfile2; then
@@ -26,10 +28,12 @@ if cmp -s $outfile1 $outfile2; then
 else
 	printf "nay\n"
 fi
-rm -f $outfile1 $outfile2
+rm -f $outfile1 $outfile2 $infile
 
 # if string1 is an empty string, do nothing
 printf "TEST 2\n"
+echo hallo > $infile
+echo hello > $infile
 ./seddy $infile "" B
 if [ -e $outfile1 ]; then
 	printf "nay\n"
@@ -40,6 +44,8 @@ rm -f $outfile1 $outfile2
 
 # replace with nothing
 printf "TEST 3\n"
+echo hallo > $infile
+echo hello > $infile
 ./seddy $infile "a" ""
 sed 's/a//g' $infile > $outfile2
 if cmp -s $outfile1 $outfile2; then
@@ -47,10 +53,12 @@ if cmp -s $outfile1 $outfile2; then
 else
 	printf "nay\n"
 fi
-rm -f $outfile1 $outfile2
+rm -f $outfile1 $outfile2 $infile
 
 # replace with a string containing space
 printf "TEST 4\n"
+echo hallo > $infile
+echo hello > $infile
 ./seddy $infile a "Baba yaya"
 sed 's/a/Baba yaya/g' $infile > $outfile2
 if cmp -s $outfile1 $outfile2; then
@@ -58,10 +66,12 @@ if cmp -s $outfile1 $outfile2; then
 else
 	printf "nay\n"
 fi
-rm -f $outfile1 $outfile2
+rm -f $outfile1 $outfile2 $infile
 
 # replace with space
 printf "TEST 5\n"
+echo hallo > $infile
+echo hello > $infile
 ./seddy $infile asd " "
 sed 's/asd/ /g' $infile > $outfile2
 if cmp -s $outfile1 $outfile2; then
@@ -69,10 +79,12 @@ if cmp -s $outfile1 $outfile2; then
 else
 	printf "nay\n"
 fi
-rm -f $outfile1 $outfile2
+rm -f $outfile1 $outfile2 $infile
 
 # no read from infile
 printf "TEST 6\n"
+echo hallo > $infile
+echo hello > $infile
 chmod -r $infile
 ./seddy $infile a B
 if [ -e $outfile1 ]; then
@@ -80,11 +92,12 @@ if [ -e $outfile1 ]; then
 else
 	printf "yay\n"
 fi
-rm -f $outfile1 $outfile2
-chmod +r $infile
+rm -f $outfile1 $outfile2 $infile
 
 # no write to outfile
 printf "TEST 7\n"
+echo hallo > $infile
+echo hello > $infile
 touch $outfile1
 chmod -w $outfile1
 ./seddy $infile a B
@@ -93,6 +106,5 @@ if [ -s $outfile1 ]; then
 else
 	printf "yay\n"
 fi
-rm -f $outfile1 $outfile2
-chmod +r $infile
+rm -f $outfile1 $outfile2 $infile
 
