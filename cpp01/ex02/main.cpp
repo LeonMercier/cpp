b290880cc76f5706858e63afb8cc6a94645b5731
lemercie <lemercie@student.hive.fi>
2025-02-06 13:14:36 +0200

    cpp01/ex02 add comments

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:43:53 by lemercie          #+#    #+#             */
-/*   Updated: 2025/01/23 17:42:59 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/06 13:14:02 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ int	main(void)
 {
 	std::string	str = "HI THIS IS BRAIN";
 	std::string	*stringPTR = &str;
+	// & means, type of object is reference, assignement to it doesnt need
+	// special operrator
 	std::string	&stringREF = str;
 
 	std::cout << &str << std::endl;
 	std::cout << stringPTR << std::endl;
+	// here & means: "get address of object (doesnt matter that it is a 
+	// referece"
 	std::cout << &stringREF << std::endl;
 
 	std::cout << str << std::endl;
