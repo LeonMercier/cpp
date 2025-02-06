f085bbf126e8637e9dafd58c03476e7e2b7db05a
lemercie <lemercie@student.hive.fi>
2025-02-06 15:48:15 +0200

    cpp01/ex06 split strings nicely

--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 12:22:53 by lemercie          #+#    #+#             */
-/*   Updated: 2025/02/06 12:22:57 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/06 15:45:14 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@ class	Harl
 private:
 	void (Harl::*_funcs[4])() = {&Harl::debug, &Harl::info, &Harl::warning,
 		&Harl::error};
+
 	void	debug(void);
 	void	info(void);
 	void	warning(void);
 	void	error(void);
 public:
 	std::string	lvl_names[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
 	void	complain(std::string level);
 };
 #endif
