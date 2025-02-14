ba31f941761eebc6e53abab36e04106351c3b9d7
lemercie <lemercie@student.hive.fi>
2025-02-14 12:11:55 +0200

    linting

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 11:53:53 by lemercie          #+#    #+#             */
-/*   Updated: 2025/01/29 13:28:59 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/14 11:53:42 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@
 
 class	ClapTrap {
 private:
-	std::string	_name;
-	unsigned int			_hitpts;
-	unsigned int			_energypts;
-	unsigned int			_attackdmg;
+	std::string		_name;
+	unsigned int	_hitpts;
+	unsigned int	_energypts;
+	unsigned int	_attackdmg;
+
 public:
 	ClapTrap(std::string name);
 	ClapTrap(const ClapTrap &source);
@@ -31,6 +32,5 @@ public:
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
-
 };
 #endif
