80dcaa10b28af85199240c714a05764b3c904822
lemercie <lemercie@student.hive.fi>
2025-02-27 12:17:44 +0200

    cleanup

--- cpp04/ex01/Brain.hpp
+++ cpp04/ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:14:21 by lemercie          #+#    #+#             */
-/*   Updated: 2025/02/17 17:50:45 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/27 12:15:23 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 #define BRAIN_HPP
 
 # include <string>
-#include <iostream>
+# include <iostream>
 
 class	Brain {
-protected:
 public:
 	std::string ideas[100];
+
 	Brain();
 	Brain(const Brain &source);
 	Brain	&operator=(const Brain &source);
