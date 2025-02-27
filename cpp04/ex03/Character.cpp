61a390dbf2bbd86cec9710ead8086c9c9018b4c3
lemercie <lemercie@student.hive.fi>
2025-02-27 16:42:03 +0200

    do proper deep copies, add tests

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 14:57:34 by lemercie          #+#    #+#             */
-/*   Updated: 2025/02/27 12:53:27 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/27 15:57:54 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,14 @@ Character::Character(const Character &source) : _name(source._name)
 {
 	for (int i = 0; i < 4; i++)
 	{
-		_inventory[i] = source._inventory[i]->clone();
+		if (source._inventory[i] != nullptr)
+		{
+			_inventory[i] = source._inventory[i]->clone();
+		}
+		else
+		{
+			_inventory[i] = nullptr;
+		}
 	}
 }
 
@@ -60,7 +67,14 @@ Character & Character::operator=(const Character &source)
 			{
 				delete _inventory[i];
 			}
-			_inventory[i] = source._inventory[i]->clone();
+			if (source._inventory[i] != nullptr)
+			{
+				_inventory[i] = source._inventory[i]->clone();
+			}
+			else
+			{
+				_inventory[i] = nullptr;
+			}
 		}
 	}
 	return (*this);
--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 17:31:30 by lemercie          #+#    #+#             */
-/*   Updated: 2025/02/20 15:48:37 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:08:31 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 #include "Character.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
+
+void	copyTest(Character cha, Character tar)
+{
+	std::cout << "is it a deep copy?" << std::endl;
+	cha.use(0, tar);
+}
 
 int main()
 {
@@ -35,10 +41,37 @@ int main()
 	me->use(0, *bob);
 	me->use(1, *bob);
 
+	Character john("John");
+	Character dave("Dave");
+	Character bork("Borko");
+	AMateria* mat = new Ice();
+	AMateria* mat2 = new Cure();
+	AMateria* mat3 = new Cure();
+	AMateria* mat4 = new Cure();
+	AMateria* mat5 = new Cure();
+	std::cout << john.getName() << std::endl;
+	std::cout << dave.getName() << std::endl;
+	dave.equip(mat);
+	dave.equip(mat2);
+	dave.use(0, bork);
+	dave.use(1, bork);
+	john = dave;
+	std::cout << john.getName() << std::endl;
+	std::cout << dave.getName() << std::endl;
+	dave.use(0, bork);
+	dave.use(1, bork);
+
+	copyTest(dave, bork);
+
+	dave.equip(mat3);
+	dave.equip(mat4);
+	dave.use(0, bork);
+	dave.equip(mat5);
+	dave.use(0, bork); // old materia stays and is not overwritten
+
 	delete bob;
 	delete me;
 	delete src;
-//TODO: confirm deep copy of Character
 
 	return (0);
 }
