eb407619b9ea9ec8f55675eb8c260ffa43f10bf7
lemercie <lemercie@student.hive.fi>
2025-04-16 17:23:34 +0300

    cpp06/ex01 make Data into a class, make tests more robust

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -1,29 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/15 12:07:43 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/16 17:18:31 by lemercie         ###   ########.fr       */
+/*   Created: 2025/04/16 17:18:37 by lemercie          #+#    #+#             */
+/*   Updated: 2025/04/16 17:19:16 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
-#include <cstdint>
 #include <string>
-#include <iostream>
-#include "Data.hpp"
 
-class Serializer {
+class Data {
 public:
-	static uintptr_t	serialize(Data *ptr);
-	static Data*		deserialize(uintptr_t raw);
-private:
-	Serializer() = delete;
-	Serializer(const Serializer &source) = delete;
-	Serializer &operator=(Serializer &source) = delete;
-	~Serializer() = delete;
+	std::string	name;
+	int			value;
 };
+
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,28 +6,32 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 12:24:11 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/15 16:53:35 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/16 17:22:15 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
 int	main(void) {
-	Data *one = new Data();
-	one->name = "Test name";
-	one->value = 42;
+	try {
+		Data* one = new Data;
+		one->name = "Test name";
+		one->value = 42;
 
-	std::cout << "Address: " << one << std::endl;
-	uintptr_t location = Serializer::serialize(one);
-	std::cout << "Address: " << "0x" << std::hex << location 
-		<< std::dec << std::endl;
-	Data *result = Serializer::deserialize(location);
-	std::cout << "Address: " << result << std::endl;
-	if (one == result) {
-		std::cout << "Success" << std::endl;
-	} else {
-		std::cout << "Failure" << std::endl;
+		std::cout << "Address: " << one << std::endl;
+		uintptr_t location = Serializer::serialize(one);
+		std::cout << "Address: " << "0x" << std::hex << location 
+			<< std::dec << std::endl;
+		Data *result = Serializer::deserialize(location);
+		std::cout << "Address: " << result << std::endl;
+		if (one == result) {
+			std::cout << "Success" << std::endl;
+		} else {
+			std::cout << "Failure" << std::endl;
+		}
+		std::cout << result->name << ": " << result->value << std::endl;
+		delete one;
+	} catch (...) {
+		std::cout << "Error: unhandled exception" << std::endl;
 	}
-	std::cout << result->name << ": " << result->value << std::endl;
-	delete one;
 }
