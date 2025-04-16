a0c07a7c431b23cdc31bcef34329c1c711deb52e
lemercie <lemercie@student.hive.fi>
2025-04-16 17:27:39 +0300

    cpp06/ex02 cast direcly to void

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:55:08 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/15 18:31:24 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/16 17:25:27 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,9 @@ Base*	generate(void) {
 		case 0:
 			std::cout << "A = ";
 			return new A;
-			break;
 		case 1:
 			std::cout << "B = ";
 			return new B;
-			break;
 		case 2:
 			std::cout << "C = ";
 			return new C;
@@ -49,19 +47,16 @@ void	identify(Base* p) {
 
 void	identify(Base& p) {
 	try {
-		A &temp = dynamic_cast<A&>(p);
+		(void) dynamic_cast<A&>(p);
 		std::cout << "A";
-		(void) temp;
 	} catch (...) {}
 	try {
-		B &temp = dynamic_cast<B&>(p);
+		(void) dynamic_cast<B&>(p);
 		std::cout << "B";
-		(void) temp;
 	} catch (...) {}
 	try {
-		C &temp = dynamic_cast<C&>(p);
+		(void) dynamic_cast<C&>(p);
 		std::cout << "C";
-		(void) temp;
 	} catch (...) {}
 	std::cout << std::endl;
 }
