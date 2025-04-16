fcd0f6ad5b47d5db990bcfe90270858e32667d78
lemercie <lemercie@student.hive.fi>
2025-04-16 16:15:02 +0300

    cpp05/ex01 prettify tests

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 16:39:53 by lemercie          #+#    #+#             */
-/*   Updated: 2025/03/28 13:29:02 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/16 16:12:24 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,34 @@
 #include "Form.hpp"
 
 int main(void) {
-	Bureaucrat	erm("Ermando", 1);
-	Bureaucrat	orm("Ormando", 150);
-	Form		fofo("Fofo", false, 50, 50);
-	Form		fafa("Fafa", false, 50, 50);
-	Form		fifi("Fifi", false, 50, 50);
-	Form		fufu("Fufu", false, 50, 50);
-	
-	erm.signForm(fofo);
-	orm.signForm(fafa);
-
 	try {
-		fifi.beSigned(erm);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << fifi.getSigned() << std::endl;
+		Bureaucrat	erm("Ermando", 1);
+		Bureaucrat	orm("Ormando", 150);
+		Form		fofo("Fofo", false, 50, 50);
+		Form		fafa("Fafa", false, 50, 50);
+		Form		fifi("Fifi", false, 50, 50);
+		Form		fufu("Fufu", false, 50, 50);
+		
+		std::cout << "### Bureaucrat::signForm()" << std::endl;
+		erm.signForm(fofo);
+		orm.signForm(fafa);
 
-	try {
-		fufu.beSigned(orm);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << fufu.getSigned() << std::endl;
+		std::cout << std::endl << "### Form::beSigned()" << std::endl;
+		try {
+			fifi.beSigned(erm);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << "Boolean: " << fifi.getSigned() << std::endl;
 
+		try {
+			fufu.beSigned(orm);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << "Boolean: " << fufu.getSigned() << std::endl;
+
+	} catch (...) {
+		std::cout << "Error: unhandled exception" << std::endl;
+	}
 }
