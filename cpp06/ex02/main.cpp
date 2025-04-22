a198d06c66d85139cad816dfc2b54c49b7c2cf51
lemercie <lemercie@student.hive.fi>
2025-04-22 13:46:00 +0300

    cpp06/ex02 cleanup

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:55:08 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/16 17:25:27 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/22 13:43:16 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ Base*	generate(void) {
 	int rnum = rand() % 3;
 	switch (rnum) {
 		case 0:
-			std::cout << "A = ";
+			std::cout << "generating: A";
 			return new A;
 		case 1:
-			std::cout << "B = ";
+			std::cout << "generating: B";
 			return new B;
 		case 2:
-			std::cout << "C = ";
+			std::cout << "generating: C";
 			return new C;
 	}
 	return nullptr;
@@ -34,48 +34,60 @@ Base*	generate(void) {
 
 void	identify(Base* p) {
 	if (dynamic_cast<A*>(p) != nullptr) {
-		std::cout << "A";
-	} else if (dynamic_cast<B*>(p) != nullptr) {
-		std::cout << "B";
-	} else if (dynamic_cast<C*>(p) != nullptr) {
-		std::cout << "C";
-	} else {
-		std::cout << "Error";
+		std::cout << "identified: A" << std::endl;
+		return ;
 	}
-	std::cout << std::endl;
+	if (dynamic_cast<B*>(p) != nullptr) {
+		std::cout << "identified: B" << std::endl;
+		return ;
+	}
+	if (dynamic_cast<C*>(p) != nullptr) {
+		std::cout << "identified: C" << std::endl;
+		return ;
+	}
+	std::cout << "Could not identify" << std::endl;
 }
 
 void	identify(Base& p) {
 	try {
 		(void) dynamic_cast<A&>(p);
-		std::cout << "A";
+		std::cout << "identified: A" << std::endl;
+		return ;
 	} catch (...) {}
 	try {
 		(void) dynamic_cast<B&>(p);
-		std::cout << "B";
+		std::cout << "identified: B" << std::endl;
+		return ;
 	} catch (...) {}
 	try {
 		(void) dynamic_cast<C&>(p);
-		std::cout << "C";
+		std::cout << "identified: C" << std::endl;
+		return ;
 	} catch (...) {}
-	std::cout << std::endl;
+	std::cout << "Could not identify" << std::endl;
 }
 
 int main(void) {
-	std::srand(time(NULL));
-	Base *test;
+	try {
+		std::srand(time(NULL));
+		Base *test;
 
-	std::cout << "testing pointers" << std::endl;
-	for (int i = 0; i < 10; i++) {
-		test = generate();
-		identify(test);
-		delete test;
-	}
+		std::cout << "testing pointers" << std::endl;
+		for (int i = 0; i < 10; i++) {
+			test = generate();
+			std::cout << ", ";
+			identify(test);
+			delete test;
+		}
 
-	std::cout << "testing references" << std::endl;
-	for (int i = 0; i < 10; i++) {
-		test = generate();
-		identify(*test);
-		delete test;
+		std::cout << "testing references" << std::endl;
+		for (int i = 0; i < 10; i++) {
+			test = generate();
+			std::cout << ", ";
+			identify(*test);
+			delete test;
+		}
+	} catch (std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
 }
