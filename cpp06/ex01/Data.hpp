166157015dac5e30cc439ecfe56ad1139c1dad59
lemercie <lemercie@student.hive.fi>
2025-04-22 12:58:33 +0300

    cpp06/ex01 add canonical form to Data class

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 17:18:37 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/16 17:19:16 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/22 12:57:55 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@ class Data {
 public:
 	std::string	name;
 	int			value;
+
+	Data() = default;
+	Data(Data &source) = default;
+	Data &operator=(Data &source) = default;
+	~Data() = default;
 };
 
