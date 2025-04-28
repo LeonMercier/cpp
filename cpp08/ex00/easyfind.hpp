fd27edaed1219f5d5a0cee795cc7b48a86e830bf
lemercie <lemercie@student.hive.fi>
2025-04-28 12:07:16 +0300

    cpp08/ex00 improve tests

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,12 +6,14 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 13:16:29 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/25 13:54:21 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/28 12:04:15 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+// if this function were to take a const reference to container as parameter,
+// then it would also have to return a T::const_iterator
 template <typename T>
 typename T::iterator easyfind(T &cont, const int to_find) {
 	for (auto iter = cont.begin(); iter != cont.end(); iter++) {
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 13:33:32 by lemercie          #+#    #+#             */
-/*   Updated: 2025/04/25 13:57:08 by lemercie         ###   ########.fr       */
+/*   Updated: 2025/04/28 11:58:07 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,84 @@
 #include "easyfind.hpp"
 
 int	main(void) {
-	std::vector<int> vec = {1, 2, 3, 4, 5};
-
 	std::cout << "Find element in vector" << std::endl;
-	std::cout << *easyfind(vec, 3) << std::endl;
+	std::vector<int> vec = {1, 2, 3, 4, 5};
+	auto found1 = easyfind(vec, 3);
+	if (found1 != vec.end()) {
+		std::cout << *found1 << std::endl;
+	} else {
+		std::cout << "Not found" << std::endl;
+	}
 
-	std::cout << "we can alter the element despite the iterator being const"
+	std::cout << "we can alter the element using the returned iterator"
 		<< std::endl;
-	auto elem = easyfind(vec, 4);
-	std::cout << *elem << std::endl;
-	(*elem)++;
-	std::cout << *elem << std::endl;
+	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
+		std::cout << *iter << ", ";
+	}
+	std::cout << std::endl;
+
+	(*found1)++;
+
+	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
+		std::cout << *iter << ", ";
+	}
+	std::cout << std::endl << std::endl;
 
 
-	std::cout << std::endl << "Find element in deque" << std::endl;
+	std::cout << "Find element in deque" << std::endl;
 	std::deque<int> deq = {1, 2, 3, 4, 5};
-	std::cout << *easyfind(deq, 3) << std::endl;
+	auto found2 = easyfind(deq, 1);
+	if (found2 != deq.end()) {
+		std::cout << *found1 << std::endl;
+	} else {
+		std::cout << "Not found" << std::endl;
+	}
 
-	std::cout << "we can alter the element despite the iterator being const"
+	std::cout << "we can alter the element using the returned iterator"
 		<< std::endl;
-	auto elem2 = easyfind(deq, 5);
-	std::cout << *elem2 << std::endl;
-	(*elem2)++;
-	std::cout << *elem2 << std::endl;
+	for (auto iter = deq.begin(); iter != deq.end(); iter++) {
+		std::cout << *iter << ", ";
+	}
+	std::cout << std::endl;
+
+	(*found2)++;
+
+	for (auto iter = deq.begin(); iter != deq.end(); iter++) {
+		std::cout << *iter << ", ";
+	}
+	std::cout << std::endl << std::endl;
 
 
-	std::cout << std::endl << "Find element in list" << std::endl;
-	std::list<int> lst = {1, 2, 3, 4, 5};
-	std::cout << *easyfind(lst, 3) << std::endl;
+	{ //scope
+		std::cout << "Find element in list" << std::endl;
+		std::list<int> lst = {1, 2, 3, 4, 5};
+		auto found3 = easyfind(lst, 5);
+		if (found3 != lst.end()) {
+			std::cout << *found3 << std::endl;
+		} else {
+			std::cout << "Not found" << std::endl;
+		}
+		std::cout << "we can alter the element using the returned iterator"
+			<< std::endl;
+		for (auto iter = lst.begin(); iter != lst.end(); iter++) {
+			std::cout << *iter << ", ";
+		}
+		std::cout << std::endl;
+		(*found3)++;
+		for (auto iter = lst.begin(); iter != lst.end(); iter++) {
+			std::cout << *iter << ", ";
+		}
+		std::cout << std::endl << std::endl;
+	}
 
-	std::cout << "we can alter the element despite the iterator being const"
-		<< std::endl;
-	auto elem3 = easyfind(lst, 5);
-	std::cout << *elem3 << std::endl;
-	(*elem3)++;
-	std::cout << *elem3 << std::endl;
-	std::cout << *easyfind(lst, 5) << std::endl;
+	{ // scope
+		std::cout << "Element not in list" << std::endl;
+		std::list<int> lst = {1, 2, 3, 4, 5};
+		auto found3 = easyfind(lst, 6);
+		if (found3 != lst.end()) {
+			std::cout << *found3 << std::endl;
+		} else {
+			std::cout << "Not found" << std::endl;
+		}
+	}
 }
