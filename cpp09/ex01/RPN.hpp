ead56751ffea5fd7641b2ef965b842376c367cc9
lemercie <lemercie@student.hive.fi>
2025-05-08 16:38:18 +0300

    add missing include guard

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
 #include <sstream>
 #include <stack>
